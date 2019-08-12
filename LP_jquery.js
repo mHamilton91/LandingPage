338022129a2918077f7424c97fe07bb633435654
mHamilton91 <m.hamilton2891@gmail.com>
2019-08-12 18:58:01 -0500

    Updating and adding files

---
+++ LP_jquery.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+	//Tip Calculator
+	$("#tipCalcScreenshot").mouseenter(function() {
+		$("#tipCalcDes").stop(true, true).slideDown("slow"); //Stop used to keep the animation from playing all the way through multiple times if hovered over quickly multiple times
+	});
+	
+	$("#tipDiv").mouseleave(function() {
+		$("#tipCalcDes").slideUp("slow");
+	});
+	
+	//Clock
+	$("#clockScreenshot").mouseenter(function() {
+		$("#clockDes").stop(true, true).slideDown("slow");
+	});
+	
+	$("#clockDiv").mouseleave(function() {
+		$("#clockDes").slideUp("slow");
+	});
+	
+	//Lumi
+	$("#lumiScreenshot").mouseenter(function() {
+		$("#lumiDes").stop(true, true).slideDown("slow");
+	});
+	
+	$("#lumiDiv").mouseleave(function() {
+		$("#lumiDes").slideUp("slow");
+	});
+	
+	//Drum Kit
+	$("#drumKitScreenshot").mouseenter(function() {
+		$("#drumKitDes").stop(true, true).slideDown("slow");
+	});
+	
+	$("#drumKitDiv").mouseleave(function() {
+		$("#drumKitDes").slideUp("slow");
+	});
+	
+	//Leona
+	$("#leonaScreenshot").mouseenter(function() {
+		$("#leonaDes").stop(true, true).slideDown("slow");
+	});
+	
+	$("#leonaDiv").mouseleave(function() {
+		$("#leonaDes").slideUp("slow");
+	});
+});
